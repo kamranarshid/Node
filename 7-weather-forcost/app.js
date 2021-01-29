1a78c34993be7bc10b7a1bd26a15377549c12906
Muhammad Kamran Arshid <77382231+kamranarshid@users.noreply.github.com>
2021-01-29 11:48:18 +0500

    Weather forcost

--- 7-weather-forcost/app.js
+++ 7-weather-forcost/app.js
@@ -1,11 +1,12 @@
 const request = require('postman-request')
 const postMaster = require('postman-request')
 
-const url = "http://api.weatherstack.com/current?access_key=562f9b5a83f080e5e0f02acb136917ff&%20query=islamabad"
+const url = "http://api.weatherstack.com/current?access_key=562f9b5a83f080e5e0f02acb136917ff&%20query=islamabad&units=f"
 
 
-request({ url: url} , (error, response)=>{
-        const data = JSON.parse(response.body)
-        console.log(data.current)
+request({ url: url , json: true} , (error, response)=>{
+        ///console.log(response.body.current)
+
+        console.log(response.body.current.weather_descriptions[0] +". Its currently "+ response.body.current.temperature +" degree our there is "+response.body.current.precip+" %  chance of rain")
 })
 //http://api.weatherstack.com/current?access_key=562f9b5a83f080e5e0f02acb136917ff&%20query=islamabad
